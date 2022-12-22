c0eabed587418cb11d44b37aec9ef7db4b659791
KJM <kjm99d@naver.com>
2022-12-22 09:36:30 +0900

    Project Initialize

---
+++ src/main/java/com/project/freeboard/FreeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.project.freeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/freeboard/FreeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.freeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
